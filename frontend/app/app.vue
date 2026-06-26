b4b8c32befcc0a3e67bc23e633e6139f99b79888
hiiD4vd <mahezadaudabubakar@gmail.com>
2026-06-26 11:25:58 +0700

    fitur(ui): menyelesaikan desain neobrutalism dan mengubah data dummy ke bahasa indonesia

--- frontend/app/app.vue
+++ frontend/app/app.vue
@@ -3,7 +3,7 @@
     <!-- Global Nav -->
     <nav class="bg-neo-yellow border-b-neo border-neo-black h-[70px] w-full flex items-center justify-between px-4 sm:px-8 z-50 fixed top-0">
       <div class="flex items-center space-x-8 max-w-[1300px] mx-auto w-full justify-between sm:justify-start">
-        <NuxtLink to="/" class="flex items-center justify-center size-10 bg-neo-pink border-neo border-neo-black shadow-neo-sm hover:translate-x-[2px] hover:translate-y-[2px] hover:shadow-none transition-all font-black text-xl">
+        <NuxtLink to="/" class="flex items-center justify-center size-10 bg-neo-red text-white border-neo border-neo-black shadow-neo-sm hover:translate-x-[2px] hover:translate-y-[2px] hover:shadow-none transition-all font-black text-xl">
           R
         </NuxtLink>
         <div class="hidden sm:flex space-x-6 items-center">
