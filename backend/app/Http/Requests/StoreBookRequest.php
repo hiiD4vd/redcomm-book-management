68b7d9b493df2baf3d8e6f985579dddbef301186
hiiD4vd <mahezadaudabubakar@gmail.com>
2026-06-26 13:36:06 +0700

    Bug fix

--- backend/app/Http/Requests/StoreBookRequest.php
+++ backend/app/Http/Requests/StoreBookRequest.php
@@ -12,7 +12,7 @@ class StoreBookRequest extends FormRequest
      */
     public function authorize(): bool
     {
-        return false;
+        return true;
     }
 
     /**
--- backend/app/Http/Requests/UpdateAuthorRequest.php
+++ backend/app/Http/Requests/UpdateAuthorRequest.php
@@ -12,7 +12,7 @@ class UpdateAuthorRequest extends FormRequest
      */
     public function authorize(): bool
     {
-        return false;
+        return true;
     }
 
     /**
